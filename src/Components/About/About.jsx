df1a2ec5f9d5e2b4415dda45ab7d99e5e1df5ae7
B-Adish <adishbalu23@gmail.com>
2025-03-07 12:15:06 +0530

    push

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -30,8 +30,15 @@ const About = () => {
               </div>
           </div>
           <div className="port-ab">
-            <div className="ps">Professional Summary</div>
-          </div>
+            <div className="ps">Professional Summary:</div>
+            <div className="ps-p">I’m a tech enthusiast with a strong passion for building web applications that not only look great but also function seamlessly. As a Full Stack Developer, I’ve worked with technologies like HTML, CSS, JavaScript, React.js, Node.js, Express.js. Through hands-on projects, I’ve gained experience in creating dynamic websites, developing RESTful APIs, and solving real-world coding challenges.
+              <br />  
+              <br /> </div>
+        <div className="ps-p">What excites me the most about development is the problem-solving aspect—figuring out how to make things work efficiently and elegantly. I thrive on learning new things, adapting to challenges, and collaborating with like-minded people. Now, I’m eager to step into the industry, contribute my skills, and grow as a developer in a team that values innovation and creativity.
+        </div>
+        
+        </div>
+          
         </div>
       </div>
     </>
